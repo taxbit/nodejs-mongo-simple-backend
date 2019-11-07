87fbbfbf15ba0af7a90b429bc273ea267cf2c7ba
Albert <albert@MacBook-Pro-Albert.local>
2019-11-07 22:40:54 +0300

    crash test 2

--- app.js
+++ app.js
@@ -24,17 +24,18 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+      throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-      throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
